dbcae93b17a03f3ddf4b270cfc3c617537265b21
bea00811 <irinakozlovawork@gmail.com>
2023-03-21 14:28:51 +0300

    refactoring4

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,25 +1,22 @@
 import startSteps from '../index.js';
 import getRandomInt from '../randomNum.js';
 
+const findDivisor = (firstNum, secondNum) => {
+  const bigger = firstNum > secondNum ? firstNum : secondNum;
+  const smaller = firstNum > secondNum ? secondNum : firstNum;
+  if (bigger % smaller === 0) {
+    return smaller;
+  }
+  return findDivisor(smaller, bigger % smaller);
+};
 
-
-  const findDivisor= (firstNum, secondNum) => {
-    const bigger = (firstNum > secondNum) ? firstNum : secondNum;
-    const smaller = (firstNum > secondNum) ? secondNum : firstNum;
-    if (bigger % smaller === 0) { return smaller; }
-    return findDivisor(smaller, bigger % smaller);
-  };
-  
-  const generateGameGcd= () => {
-    const randomNum1 = getRandomInt(100);
-    const randomNum2 = getRandomInt(100);
-    const question = `Question: ${randomNum1} ${randomNum2}`;
-    const answer = findDivisor(randomNum1, randomNum2).toString();
-    return [question, answer];
-  };
-
-
-// };
+const generateGameGcd = () => {
+  const randomNum1 = getRandomInt(100);
+  const randomNum2 = getRandomInt(100);
+  const question = `Question: ${randomNum1} ${randomNum2}`;
+  const answer = findDivisor(randomNum1, randomNum2).toString();
+  return [question, answer];
+};
 
 const startGame = () => {
   const question = 'Find the greatest common divisor of given numbers.';
